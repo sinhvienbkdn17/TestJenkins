2a88ff993887e44dfc2e6ec6c7ad3977909c84f8
Tommy Fury <sinhvienbk13@gmail.com>
2018-09-07 17:01:57 +0700

    Init Project

---
+++ TestJenkins.Tests/TestClass.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJenkins.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator _cal;
+
+        [SetUp]
+        public void Setup()
+        {
+            _cal = new Calculator();
+        }
+
+        [Test]
+        public void OnePlusOneEqualTwo()
+        {
+            Assert.AreEqual(2, _cal.Add(1, 1));
+        }
+
+        [Test]
+        public void TwoPlusTwoEqualFour()
+        {
+            Assert.AreEqual(4, _cal.Add(2, 2));
+        }
+
+        [Test]
+        public void FourPlusOneEqualFive()
+        {
+            Assert.AreEqual(5, _cal.Add(4, 1));
+        }
+    }
+}
---
+++ TestJenkins/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestJenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+    }
+}
